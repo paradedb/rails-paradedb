2b7e3be5373ad0e3c5f65014a3bd112388eda328
Isaac Van Doren <isaacjvandoren@gmail.com>
2026-08-13 14:35:47 -0500

    Drop table in examples to avoid conflict

--- examples/quickstart/setup.rb
+++ examples/quickstart/setup.rb
@@ -43,6 +43,7 @@ module QuickstartSetup
 
     conn = ActiveRecord::Base.connection
     conn.execute("CREATE EXTENSION IF NOT EXISTS pg_search CASCADE;")
+    conn.drop_table(:mock_items, if_exists: true)
     conn.execute(
       "CALL paradedb.create_bm25_test_table(schema_name => 'public', table_name => 'mock_items');"
     )
